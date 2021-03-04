66d4ba8ab8caf73a71ebfa3c787e2f0844493d8b
Rafael Ferrés <rafael.ferres-jum@parceiros-kroton.com.br>
2021-03-04 17:23:24 -0300

    Create rabbitmq class

---
+++ src/services/rabbitmq.js
@@ -0,0 +1,26 @@
+const amqp = require('amqplib')
+
+class RabbitMq {
+    constructor(stringConenction, queue, exchangeName, pattern){
+        this.exchangeName = exchangeName;
+        this.pattern = pattern;
+        this._stringConnection = `${stringConenction}`;
+        this.queue = queue;
+    }
+
+    async listen(callBack){
+        this.connection = await amqp.connect(this._stringConnection)
+        this.channel = await this.connection.createChannel()
+
+        await this.channel.assertQueue(this.queue)
+        await this.channel.assertExchange(this.exchangeName, 'direct')
+
+        await this.channel.bindQueue(this.queue, this.exchangeName, this.pattern)
+
+        this.channel.consume(this.queue, callBack);
+    }
+
+
+}
+
+module.exports = RabbitMq;
